54959d427a6c4245bb5679954c972a0b468c3cc1
Luka <luka@arthaus.info>
2021-09-07 12:49:35 +0200

    updated whitelisting test

--- test/whitelistTest.js
+++ test/whitelistTest.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 const { PASSED_URI, TOKEN_NAME, TOKEN_SYMBOL, OPENSEA_PROXY_ADDRESS } = require("./testContstants.js")
 
 describe("NFTContract", function () {
-  it("Should whitelist addr1", async function () {
+  it("Should whitelist addr1 and deWhitelist it after", async function () {
 	//   getSigners() returns list of accounts from connected chain
     const [deployer, addr1] = await ethers.getSigners();
 
@@ -19,11 +19,19 @@ describe("NFTContract", function () {
 
 	// whitelisting addr1
 	await myNFT.whitelistUser(addr1.address);
-
+	
 	// after whitelisting, addr1 should be able to buy without passing msg.value
 	await myNFT.connect(addr1).mint(addr1.address, 5);	
 	const balanceOfAddr1 = await myNFT.balanceOf(addr1.address);
 	expect(balanceOfAddr1).to.equal(5);
+	// for public properties of smart contracts, solidity automatically creates getters
+	// also assert that whitelisted eq true
+	expect(await myNFT.whitelisted(addr1.address)).to.equal(true);
+	
+	// remove addr1 from whitelisting
+	await myNFT.removeWhitelistUser(addr1.address);
+
+	expect(await myNFT.whitelisted(addr1.address)).to.equal(false);
 	
   });
 });
