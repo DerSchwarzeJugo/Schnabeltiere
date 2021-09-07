5956c176d005c3f8c3f55ecd20ff82970698132f
Luka <luka@arthaus.info>
2021-09-07 18:19:09 +0200

    updated transferTests

--- test/transferTest.js
+++ test/transferTest.js
@@ -5,7 +5,7 @@ const { PASSED_URI, TOKEN_NAME, TOKEN_SYMBOL, OPENSEA_PROXY_ADDRESS } = require(
 describe("NFTContract", function () {
   it("Should transfer token #15 to addr1", async function () {
 	//   getSigners() returns list of accounts from connected chain
-    const [deployer, addr1] = await ethers.getSigners();
+    const [deployer, addr1, addr2] = await ethers.getSigners();
 
 	console.log("Deploying contracts with the account:", deployer.address);
 
@@ -13,12 +13,11 @@ describe("NFTContract", function () {
     // Start deployment, returning a promise that resolves to a contract object
 	const myNFT = await MyNFT.deploy(PASSED_URI, TOKEN_NAME, TOKEN_SYMBOL, OPENSEA_PROXY_ADDRESS);
     await myNFT.deployed();
-	console.log("Should transfer item to addr1", myNFT.address);
-
+	console.log("Contract deployed to address:", myNFT.address);
     expect(await myNFT.baseTokenURI()).to.equal(PASSED_URI);
 
 	// transfer item with id 15 to addr1
-	// safeTransferFrom is overloaded function (from erc721), this has diffrent syntax
+	// approve & safeTransferFrom are overloaded functions (from erc721), this has diffrent syntax
 	await myNFT["safeTransferFrom(address,address,uint256)"](deployer.address, addr1.address, 15);
 	const balanceOfAddr1 = await myNFT.balanceOf(addr1.address);
 	expect(balanceOfAddr1).to.equal(1);
@@ -32,5 +31,13 @@ describe("NFTContract", function () {
 	}
 
 	expect(convertedArr[0]).to.equal(15)
+
+	await myNFT["safeTransferFrom(address,address,uint256)"](deployer.address, addr2.address, 11);
+	
+	await myNFT.connect(addr2)["safeTransferFrom(address,address,uint256)"](addr2.address, addr1.address, 11);
+
+	const balanceOfAddr2 = await myNFT.balanceOf(addr2.address);
+	expect(balanceOfAddr2).to.equal(0);
+
   });
 });
